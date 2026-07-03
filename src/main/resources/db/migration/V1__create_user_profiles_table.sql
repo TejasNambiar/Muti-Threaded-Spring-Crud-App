d02aba9d9402c907bb160de94ccaa75ed2a50eab
Tejas Nambiar <justanotherapp-1@outlook.com>
2026-07-03 15:17:49 +0530

    added flyway for sql db

---
+++ src/main/resources/db/migration/V1__create_user_profiles_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user_profiles
+(
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(255)        NOT NULL,
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    department VARCHAR(255)
+);
