b8db63ec7f4862256e5820deb6ff17f13ac9d72d
Tejas Nambiar <justanotherapp-1@outlook.com>
2026-07-03 14:24:40 +0530

    base

---
+++ src/test/java/com/userdashboard/mutithreadedspringcrudapp/MutiThreadedSpringCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userdashboard.mutithreadedspringcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutiThreadedSpringCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
